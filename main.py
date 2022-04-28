a46f33893206ac2783075794f6a90c8a236f5ca9
Ruslan <rusl.babae@yandex.ru>
2022-04-29 00:04:01 +0300

    start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 import json
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_ngrok import run_with_ngrok
 from data import db_session
 from data.users import User
@@ -12,40 +12,36 @@ run_with_ngrok(app)
 logging.basicConfig(level=logging.INFO)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
-@app.route('/start', methods=['POST'])
-def start():
-    logging.info(f'Request: {request.json!r}')
-
+@app.route('/start/<user_id>')
+def start(user_id):
     db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.user_id == request.json['user_id']).first()
+    user = db_sess.query(User).filter(User.user_id == user_id).first()
     if user:
         response = {
-            'user': user.user_id,
-            'chapter': user.chapter.title,
-            'event': user.event.content,
-            'new_user': False
+            "user": user.user_id,
+            "chapter": user.chapter_id,
+            "event": user.event_id
         }
         logging.info(f'Response: {response!r}')
-        return json.dumps(response, ensure_ascii=False)
+        return response
+    return abort(404)
 
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    logging.info(f'Request: {request.json!r}')
+    db_sess = db_session.create_session()
     user = User(user_id=request.json['user_id'])
     db_sess.add(user)
     db_sess.commit()
     user = db_sess.query(User).filter(User.user_id == request.json['user_id']).first()
-
     response = {
-        'user': user.user_id,
-        'chapter': user.chapter.title,
-        'event': user.event.content,
-        'new_user': True
+        "user": user.user_id,
+        "chapter": user.chapter_id,
+        "event": user.event_id
     }
     logging.info(f'Response: {response!r}')
-    return json.dumps(response, ensure_ascii=False)
+    return response
 
 
 if __name__ == '__main__':
