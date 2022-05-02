af434094fdd85ff0d193b8e008cfe06c619fb450
Ruslan <rusl.babae@yandex.ru>
2022-05-03 02:52:46 +0300

    greeting_4

--- handlers.py
+++ handlers.py
@@ -38,7 +38,7 @@ def dialog_handler(req, res):
         for item in data['events'][res['user_state_update']['event']]['items']:
             res['user_state_update']['items'].append(item)
     res['response']['text'] = data['events'][res['user_state_update']['event']]['text']
-    res['response']['tts'] = res['text']
+    res['response']['tts'] = res['response']['text']
     res['response']['buttons'] = data['events'][res['user_state_update']['event']]['buttons']
     return res
 
