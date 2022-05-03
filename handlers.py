e7bd36cfd475b7bfddf4da34cd36db54cddb6d31
Ruslan <rusl.babae@yandex.ru>
2022-05-03 16:56:05 +0300

    greeting_9

--- handlers.py
+++ handlers.py
@@ -10,57 +10,7 @@ KARMA = {-3: 'Демоническая', -2: 'Дурная', -1: 'Негатив
 
 
 def dialog_handler(req, res):
-    try:
-        if not req['state']['user']:
-            res['user_state_update'] = {
-                'chapter': 'start',
-                'event': 'greeting',
-                'reputation': 0,
-                'mood': 0,
-                'karma': 0,
-                'items': []
-            }
-            data = data_handler('start')
-        elif req['request']['nlu']['intents'] and req['state']['user']['chapter'] != 'start':
-            res['user_state_update'] = req['state']['user']
-            return intent_handler(res, list(req['request']['nlu']['intents'].keys())[0])
-        else:
-            res['user_state_update'] = req['state']['user']
-            data = data_handler(req['state']['user']['chapter'])
-            if data == 'Error':
-                raise Exception
-            if req['request']['payload']:
-                if data['events'][req['state']['user']['event']]['last_event']:
-                    res['user_state_update']['event'] = data['events'][req['state']['user']['event']]['next_event'][0][
-                        'event']
-                    data = data_handler(data['next_chapter'])
-                elif not req['request']['payload']['random']:
-                    res['user_state_update']['event'] = req['request']['payload']['next_event'][0]['event']
-                else:
-                    res['user_state_update']['event'] = random.choice(req['request']['payload']['next_event'])['event']
-            else:
-                res['user_state_update']['event'] = answer_handler(req, data['events'][req['state']['user']['event']],
-                                                                   list(req['request']['nlu']['tokens']))
-
-            res['user_state_update']['reputation'] += data['events'][res['user_state_update']['event']]['stats'][
-                'reputation']
-            res['user_state_update']['mood'] += data['events'][res['user_state_update']['event']]['stats']['mood']
-            res['user_state_update']['karma'] += data['events'][res['user_state_update']['event']]['stats']['karma']
-            for item in data['events'][res['user_state_update']['event']]['items']:
-                res['user_state_update']['items'].append(item)
-        if res['user_state_update']['event'] == req['state']['user']['event']:
-            print(res['user_state_update']['event'])
-            print(req['state']['user']['event'])
-            res['response']['text'] = f"Прошу прощения, ответьте конкретнее.\n\n" \
-                                      f"{data['events'][res['user_state_update']['event']]['text']}"
-        else:
-            res['response']['text'] = data['events'][res['user_state_update']['event']]['text']
-        res['response']['tts'] = res['response']['text']
-        res['response']['buttons'] = data['events'][res['user_state_update']['event']]['buttons']
-
-        return res
-
-    except Exception:
+    if not req['state']['user']:
         res['user_state_update'] = {
             'chapter': 'start',
             'event': 'greeting',
@@ -70,18 +20,47 @@ def dialog_handler(req, res):
             'items': []
         }
         data = data_handler('start')
+    elif req['request']['nlu']['intents'] and req['state']['user']['chapter'] != 'start':
+        res['user_state_update'] = req['state']['user']
+        return intent_handler(res, list(req['request']['nlu']['intents'].keys())[0])
+    else:
+        res['user_state_update'] = req['state']['user']
+        data = data_handler(req['state']['user']['chapter'])
+        if req['request']['payload']:
+            if data['events'][req['state']['user']['event']]['last_event']:
+                res['user_state_update']['event'] = data['events'][req['state']['user']['event']]['next_event'][0][
+                    'event']
+                data = data_handler(data['next_chapter'])
+            elif not req['request']['payload']['random']:
+                res['user_state_update']['event'] = req['request']['payload']['next_event'][0]['event']
+            else:
+                res['user_state_update']['event'] = random.choice(req['request']['payload']['next_event'])['event']
+        else:
+            res['user_state_update']['event'] = answer_handler(req, data['events'][req['state']['user']['event']],
+                                                               list(req['request']['nlu']['tokens']))
+
+        res['user_state_update']['reputation'] += data['events'][res['user_state_update']['event']]['stats'][
+            'reputation']
+        res['user_state_update']['mood'] += data['events'][res['user_state_update']['event']]['stats']['mood']
+        res['user_state_update']['karma'] += data['events'][res['user_state_update']['event']]['stats']['karma']
+        for item in data['events'][res['user_state_update']['event']]['items']:
+            res['user_state_update']['items'].append(item)
+    if res['user_state_update']['event'] == req['state']['user']['event']:
+        print(res['user_state_update']['event'])
+        print(req['state']['user']['event'])
+        res['response']['text'] = f"Прошу прощения, ответьте конкретнее.\n\n" \
+                                  f"{data['events'][res['user_state_update']['event']]['text']}"
+    else:
         res['response']['text'] = data['events'][res['user_state_update']['event']]['text']
-        res['response']['tts'] = res['response']['text']
-        res['response']['buttons'] = data['events'][res['user_state_update']['event']]['buttons']
-        return res
+    res['response']['tts'] = res['response']['text']
+    res['response']['buttons'] = data['events'][res['user_state_update']['event']]['buttons']
+
+    return res
 
 
 def data_handler(chapter):
-    try:
-        with open(f'data/events/{chapter}.json') as json_file:
-            return json.load(json_file)
-    except FileNotFoundError:
-        return 'Error'
+    with open(f'data/events/{chapter}.json') as json_file:
+        return json.load(json_file)
 
 
 def intent_handler(res, intent):
