a97d3254655e3de814b7b5fb69dbac4721b9295e
Ruslan <rusl.babae@yandex.ru>
2022-05-04 12:03:35 +0300

    to_prologue

--- handlers.py
+++ handlers.py
@@ -11,7 +11,64 @@ intents = ['YANDEX.HELP', 'YANDEX.REJECT', 'description', 'inventory', 'stats', 
 
 
 def dialog_handler(req, res):
-    if not req['state']['user']:
+    try:
+        if not req['state']['user']:
+            res['user_state_update'] = {
+                'chapter': 'start',
+                'event': 'greeting',
+                'reputation': 0,
+                'mood': 0,
+                'karma': 0,
+                'items': []
+            }
+            data = data_handler('start')
+            res['response']['text'] = data['events'][res['user_state_update']['event']]['text']
+        elif req['request']['nlu']['intents'] and list(req['request']['nlu']['intents'].keys())[0] == 'YANDEX.REAPET':
+            data = data_handler(req['state']['user']['chapter'])
+            res['user_state_update'] = req['state']['user'].copy()
+            res['response']['text'] = data['events'][res['user_state_update']['event']]['text']
+            res['response']['tts'] = res['response']['text']
+            res['response']['buttons'] = data['events'][res['user_state_update']['event']]['buttons']
+            return res
+        elif req['request']['nlu']['intents'] and list(req['request']['nlu']['intents'].keys())[0] == 'rules' and \
+                req['state']['user']['chapter'] != 'start':
+            res['user_state_update'] = req['state']['user'].copy()
+            return intent_handler(res, list(req['request']['nlu']['intents'].keys())[0])
+        elif req['request']['nlu']['intents'] and list(req['request']['nlu']['intents'].keys())[0] in intents and \
+                list(req['request']['nlu']['intents'].keys())[0] != 'rules':
+            res['user_state_update'] = req['state']['user'].copy()
+            return intent_handler(res, list(req['request']['nlu']['intents'].keys())[0])
+        else:
+            res['user_state_update'] = req['state']['user'].copy()
+            data = data_handler(req['state']['user']['chapter'])
+            if req['request']['type'] == 'ButtonPressed':
+                if data['events'][req['state']['user']['event']]['last_event']:
+                    res['user_state_update']['event'] = data['events'][req['state']['user']['event']]['next_events'][0][
+                        'event']
+                    data = data_handler(data['next_chapter'])
+                elif not req['request']['payload']['random']:
+                    res['user_state_update']['event'] = req['request']['payload']['next_event'][0]['event']
+                else:
+                    res['user_state_update']['event'] = random.choice(req['request']['payload']['next_event'])['event']
+            else:
+                res['user_state_update']['event'] = answer_handler(req, data['events'][req['state']['user']['event']],
+                                                                   req['request']['original_utterance'])
+
+            res['user_state_update']['reputation'] += data['events'][res['user_state_update']['event']]['stats'][
+                'reputation']
+            res['user_state_update']['mood'] += data['events'][res['user_state_update']['event']]['stats']['mood']
+            res['user_state_update']['karma'] += data['events'][res['user_state_update']['event']]['stats']['karma']
+            for item in data['events'][res['user_state_update']['event']]['items']:
+                res['user_state_update']['items'].append(item)
+            if res['user_state_update']['event'] == req['state']['user']['event'] and req['session']['message_id']:
+                res['response']['text'] = f"Прошу прощения, ответьте конкретнее.\n\n" \
+                                          f"{data['events'][res['user_state_update']['event']]['text']}"
+            else:
+                res['response']['text'] = data['events'][res['user_state_update']['event']]['text']
+        res['response']['tts'] = res['response']['text']
+        res['response']['buttons'] = data['events'][res['user_state_update']['event']]['buttons']
+        return res
+    except KeyError:
         res['user_state_update'] = {
             'chapter': 'start',
             'event': 'greeting',
@@ -22,51 +79,9 @@ def dialog_handler(req, res):
         }
         data = data_handler('start')
         res['response']['text'] = data['events'][res['user_state_update']['event']]['text']
-    elif req['request']['nlu']['intents'] and list(req['request']['nlu']['intents'].keys())[0] == 'YANDEX.REAPET':
-        data = data_handler(req['state']['user']['chapter'])
-        res['user_state_update'] = req['state']['user'].copy()
-        res['response']['text'] = data['events'][res['user_state_update']['event']]['text']
         res['response']['tts'] = res['response']['text']
         res['response']['buttons'] = data['events'][res['user_state_update']['event']]['buttons']
         return res
-    elif req['request']['nlu']['intents'] and list(req['request']['nlu']['intents'].keys())[0] == 'rules' and \
-            req['state']['user']['chapter'] != 'start':
-        res['user_state_update'] = req['state']['user'].copy()
-        return intent_handler(res, list(req['request']['nlu']['intents'].keys())[0])
-    elif req['request']['nlu']['intents'] and list(req['request']['nlu']['intents'].keys())[0] in intents and \
-            list(req['request']['nlu']['intents'].keys())[0] != 'rules':
-        res['user_state_update'] = req['state']['user'].copy()
-        return intent_handler(res, list(req['request']['nlu']['intents'].keys())[0])
-    else:
-        res['user_state_update'] = req['state']['user'].copy()
-        data = data_handler(req['state']['user']['chapter'])
-        if req['request']['type'] == 'ButtonPressed':
-            if data['events'][req['state']['user']['event']]['last_event']:
-                res['user_state_update']['event'] = data['events'][req['state']['user']['event']]['next_events'][0][
-                    'event']
-                data = data_handler(data['next_chapter'])
-            elif not req['request']['payload']['random']:
-                res['user_state_update']['event'] = req['request']['payload']['next_event'][0]['event']
-            else:
-                res['user_state_update']['event'] = random.choice(req['request']['payload']['next_event'])['event']
-        else:
-            res['user_state_update']['event'] = answer_handler(req, data['events'][req['state']['user']['event']],
-                                                               req['request']['original_utterance'])
-
-        res['user_state_update']['reputation'] += data['events'][res['user_state_update']['event']]['stats'][
-            'reputation']
-        res['user_state_update']['mood'] += data['events'][res['user_state_update']['event']]['stats']['mood']
-        res['user_state_update']['karma'] += data['events'][res['user_state_update']['event']]['stats']['karma']
-        for item in data['events'][res['user_state_update']['event']]['items']:
-            res['user_state_update']['items'].append(item)
-        if res['user_state_update']['event'] == req['state']['user']['event'] and req['session']['message_id']:
-            res['response']['text'] = f"Прошу прощения, ответьте конкретнее.\n\n" \
-                                      f"{data['events'][res['user_state_update']['event']]['text']}"
-        else:
-            res['response']['text'] = data['events'][res['user_state_update']['event']]['text']
-    res['response']['tts'] = res['response']['text']
-    res['response']['buttons'] = data['events'][res['user_state_update']['event']]['buttons']
-    return res
 
 
 def data_handler(chapter):
